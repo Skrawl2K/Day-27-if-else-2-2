9baa9a608d6d8c9eab85ad481428fb65c2a10b1c
Skrawl2K <kev-koslowski@gmx.de>
2022-10-27 16:25:39 +0200

    1

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+function checkAirQuality(){
+
+// Part One 
+const airQuality = parseInt(document.getElementById ("aqi").value);
+
+let output1 ="";
+
+// Part Two
+
+
+
+
+// Result
+
+document.getElementById("output1").innerHTML = output;
+
+document.getElementById("output2").innerHTML = output;
+}
